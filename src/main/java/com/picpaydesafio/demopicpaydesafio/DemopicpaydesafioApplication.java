03bea927e1bc8cf1e5b8a2ed97d7046c396e2e8a
Lucas Cardoso <lucas.rib.card@gmail.com>
2024-10-31 14:27:48 -0300

    first commit

---
+++ src/main/java/com/picpaydesafio/demopicpaydesafio/DemopicpaydesafioApplication.java
@@ -0,0 +1,13 @@
+package com.picpaydesafio.demopicpaydesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemopicpaydesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemopicpaydesafioApplication.class, args);
+	}
+
+}
