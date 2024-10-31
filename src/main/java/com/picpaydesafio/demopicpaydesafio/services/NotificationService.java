2d54ef68dec8285df2e3bd58d86c5abe0cb0024f
Lucas Cardoso <lucas.rib.card@gmail.com>
2024-10-31 17:03:13 -0300

    feat: update NotificationService

--- src/main/java/com/picpaydesafio/demopicpaydesafio/services/NotificationService.java
+++ src/main/java/com/picpaydesafio/demopicpaydesafio/services/NotificationService.java
@@ -2,6 +2,7 @@ package com.picpaydesafio.demopicpaydesafio.services;
 
 import com.picpaydesafio.demopicpaydesafio.domain.user.User;
 import com.picpaydesafio.demopicpaydesafio.dtos.NotificationDTO;
+import com.picpaydesafio.demopicpaydesafio.services.exceptions.OfflineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,15 +15,15 @@ public class NotificationService {
   @Autowired
   private RestTemplate restTemplate;
 
-  public void sendNotification(User user, String message) throws Exception {
+  public void sendNotification(User user, String message) {
     String email = user.getEmail();
     NotificationDTO notificationRequest = new NotificationDTO(email, message);
 
+    // endpoint saiu fora do ar
     ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
 
-    if (!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
-      System.out.println("erro ao enviar notificação");
-      throw new Exception("Serviço de notificação está fora do ar.");
-    }
+    // if (!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
+    // throw new OfflineService("Serviço de notificação está fora do ar.");
+    //}
   }
 }
