6e5a1ad06f59e75efbaeaa7b0429d802b8973fa6
Lucas Cardoso <lucas.rib.card@gmail.com>
2024-10-31 15:58:29 -0300

    feat: update UserService

--- src/main/java/com/picpaydesafio/demopicpaydesafio/services/UserService.java
+++ src/main/java/com/picpaydesafio/demopicpaydesafio/services/UserService.java
@@ -2,8 +2,10 @@ package com.picpaydesafio.demopicpaydesafio.services;
 
 import com.picpaydesafio.demopicpaydesafio.domain.user.User;
 import com.picpaydesafio.demopicpaydesafio.domain.user.UserType;
+import com.picpaydesafio.demopicpaydesafio.dtos.UserDTO;
 import com.picpaydesafio.demopicpaydesafio.repositories.UserRepository;
 import java.math.BigDecimal;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +15,7 @@ public class UserService {
   @Autowired
   private UserRepository repository;
 
-  public void validateTrasaction(User sender, BigDecimal amount) throws Exception {
+  public void validateTransaction(User sender, BigDecimal amount) throws Exception {
     if (sender.getUserType() == UserType.MERCHANT) {
       throw new Exception("Usuário não está autorizado a realizar transação.");
     }
@@ -27,7 +29,17 @@ public class UserService {
     return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
   }
 
-  public void saveUser(User user) throws Exception {
+  public void saveUser(User user) {
     this.repository.save(user);
+  }
+
+  public User createUser(UserDTO data) {
+    User newUser = new User(data);
+    this.saveUser(newUser);
+    return newUser;
+  }
+
+  public List<User> getAllUsers() {
+    return this.repository.findAll();
   }
 }
