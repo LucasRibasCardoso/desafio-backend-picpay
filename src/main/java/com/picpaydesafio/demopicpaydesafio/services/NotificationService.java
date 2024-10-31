14830ac4ef86fd50c6a28ef00ea13519abaae42a
Lucas Cardoso <lucas.rib.card@gmail.com>
2024-10-31 16:03:10 -0300

    feat: update name NotificationService

--- src/main/java/com/picpaydesafio/demopicpaydesafio/services/NotificationService.java
+++ src/main/java/com/picpaydesafio/demopicpaydesafio/services/NotificationService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class notificationService {
+public class NotificationService {
 
   @Autowired
   private RestTemplate restTemplate;
