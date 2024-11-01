f7caa3bf4b44ab6a5946f7662b552563e3c6a008
Lucas Cardoso <lucas.rib.card@gmail.com>
2024-10-31 22:02:32 -0300

    feat: refactoring TransactionService

--- src/main/java/com/picpaydesafio/demopicpaydesafio/controllers/TransactionController.java
+++ src/main/java/com/picpaydesafio/demopicpaydesafio/controllers/TransactionController.java
@@ -20,7 +20,7 @@ public class TransactionController {
 
   @PostMapping
   public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transaction) throws Exception {
-    Transaction newTransaction = this.transactionService.createTransction(transaction);
+    Transaction newTransaction = this.transactionService.createTransaction(transaction);
     return new ResponseEntity<>(newTransaction, HttpStatus.CREATED);
   }
 
--- src/main/java/com/picpaydesafio/demopicpaydesafio/services/TransactionService.java
+++ src/main/java/com/picpaydesafio/demopicpaydesafio/services/TransactionService.java
@@ -1,6 +1,5 @@
 package com.picpaydesafio.demopicpaydesafio.services;
 
-import com.picpaydesafio.demopicpaydesafio.domain.transction.factory.DefaultTransactionFactory;
 import com.picpaydesafio.demopicpaydesafio.domain.transction.Transaction;
 import com.picpaydesafio.demopicpaydesafio.domain.transction.factory.TransactionFactory;
 import com.picpaydesafio.demopicpaydesafio.domain.user.User;
@@ -8,14 +7,15 @@ import com.picpaydesafio.demopicpaydesafio.dtos.TransactionDTO;
 import com.picpaydesafio.demopicpaydesafio.repositories.TransactionRepository;
 import com.picpaydesafio.demopicpaydesafio.services.exceptions.UnauthorizedTransaction;
 import com.picpaydesafio.demopicpaydesafio.services.notification.NotificationService;
-import java.math.BigDecimal;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.Map;
 
 @Service
 public class TransactionService {
@@ -24,62 +24,65 @@ public class TransactionService {
   private final TransactionRepository repository;
   private final NotificationService notificationService;
   private final RestTemplate restTemplate;
-  private TransactionFactory transactionFactor;
+  private final TransactionFactory transactionFactory;
 
   @Autowired
   public TransactionService(
-      NotificationService notificationService,
+      UserService userService,
       TransactionRepository repository,
+      NotificationService notificationService,
       RestTemplate restTemplate,
-      DefaultTransactionFactory transactionFactor,
-      UserService userService
+      TransactionFactory transactionFactory
   ) {
-    this.notificationService = notificationService;
-    this.repository = repository;
-    this.restTemplate = restTemplate;
-    this.transactionFactor = transactionFactor;
     this.userService = userService;
+    this.repository = repository;
+    this.notificationService = notificationService;
+    this.restTemplate = restTemplate;
+    this.transactionFactory = transactionFactory;
   }
 
   @Transactional
-  public Transaction createTransction(TransactionDTO transaction) {
-    User sender = this.userService.findUserById(transaction.senderId());
-    User receiver = this.userService.findUserById(transaction.receiverId());
+  public Transaction createTransaction(TransactionDTO transactionDTO) {
+    User sender = userService.findUserById(transactionDTO.senderId());
+    User receiver = userService.findUserById(transactionDTO.receiverId());
 
-    userService.validateTransaction(sender, transaction.value());
-    validadeIfSenderIsAuthorized(sender, transaction);
+    validateTransaction(sender, transactionDTO.value());
+    checkAuthorization(sender, transactionDTO.value());
 
-    Transaction newTransaction = transactionFactor.createTransaction(sender, receiver, transaction.value());
+    Transaction newTransaction = transactionFactory.createTransaction(sender, receiver, transactionDTO.value());
+    processTransfer(sender, receiver, transactionDTO.value());
 
-    sender.setBalance(sender.getBalance().subtract(transaction.value()));
-    receiver.setBalance(receiver.getBalance().add(transaction.value()));
-
-    this.repository.save(newTransaction);
-    userService.saveUser(sender);
-    userService.saveUser(receiver);
-
-    this.notificationService.sendNotification(sender, "Transação realizada com sucesso.");
-    this.notificationService.sendNotification(receiver, "Transação recebida com sucesso.");
+    persistTransactionAndUsers(newTransaction, sender, receiver);
+    notifyUsers(sender, receiver);
 
     return newTransaction;
   }
 
-  public boolean authorizedTransaction(User sender, BigDecimal value) {
-    ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://util.devi.tools/api/v2/authorize", Map.class);
-
-     if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
-       String message = (String) authorizationResponse.getBody().get("status");
-       return "success".equals(message);
-     }
-     else {
-       return false;
-     }
+  private void validateTransaction(User sender, BigDecimal amount) {
+    userService.validateTransaction(sender, amount);
   }
 
-  private void validadeIfSenderIsAuthorized(User sender, TransactionDTO transaction) {
-    boolean isAuthorized = this.authorizedTransaction(sender, transaction.value());
-    if (!isAuthorized) {
+  private void checkAuthorization(User sender, BigDecimal amount) {
+    ResponseEntity<Map> response = restTemplate.getForEntity("https://util.devi.tools/api/v2/authorize", Map.class);
+    if (!(response.getStatusCode() == HttpStatus.OK)) {
       throw new UnauthorizedTransaction("Transação não autorizada.");
+
     }
+  }
+
+  private void processTransfer(User sender, User receiver, BigDecimal amount) {
+    sender.setBalance(sender.getBalance().subtract(amount));
+    receiver.setBalance(receiver.getBalance().add(amount));
+  }
+
+  private void persistTransactionAndUsers(Transaction transaction, User sender, User receiver) {
+    repository.save(transaction);
+    userService.saveUser(sender);
+    userService.saveUser(receiver);
+  }
+
+  private void notifyUsers(User sender, User receiver) {
+    notificationService.sendNotification(sender, "Transação realizada com sucesso.");
+    notificationService.sendNotification(receiver, "Transação recebida com sucesso.");
   }
 }
