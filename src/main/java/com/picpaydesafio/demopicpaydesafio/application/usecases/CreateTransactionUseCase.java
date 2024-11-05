b31dd0c8386d56fea6c9b2ab0ec48db0a76112c9
Lucas Cardoso <lucas.rib.card@gmail.com>
2024-11-05 19:24:45 -0300

    feat: create CreateTransactionUseCase

---
+++ src/main/java/com/picpaydesafio/demopicpaydesafio/application/usecases/CreateTransactionUseCase.java
@@ -0,0 +1,30 @@
+package com.picpaydesafio.demopicpaydesafio.application.usecases;
+
+import com.picpaydesafio.demopicpaydesafio.application.transaction.validators.TransactionValidator;
+import com.picpaydesafio.demopicpaydesafio.domain.transction.factory.TransactionFactory;
+import com.picpaydesafio.demopicpaydesafio.domain.transction.model.Transaction;
+import com.picpaydesafio.demopicpaydesafio.domain.transction.repository.TransactionRepository;
+import com.picpaydesafio.demopicpaydesafio.web.transaction.dtos.TransactionRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CreateTransactionUseCase {
+
+  private final TransactionFactory transactionFactory;
+  private final TransactionValidator validator;
+  private final TransactionRepository transactionRepository;
+  private final UpdateUserBalancesAfterTransactionUseCase updateUserBalanceUseCase;
+
+  public Transaction execute(TransactionRequestDTO request) {
+    Transaction transaction = transactionFactory.createDomain(request);
+    validator.validate(transaction);
+
+    Transaction processedTransaction = transaction.process();
+    updateUserBalanceUseCase.execute(processedTransaction.getSender(), processedTransaction.getReceiver());
+
+    return transactionRepository.save(processedTransaction);
+
+  }
+}
