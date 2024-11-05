7b84ae348b21bcf1d4c63d0dde0cf1641562ce6f
Lucas Cardoso <lucas.rib.card@gmail.com>
2024-11-05 19:25:39 -0300

    feat: create SendEmailUseCase

---
+++ src/main/java/com/picpaydesafio/demopicpaydesafio/application/usecases/SendEmailUseCase.java
@@ -0,0 +1,17 @@
+package com.picpaydesafio.demopicpaydesafio.application.usecases;
+
+import com.picpaydesafio.demopicpaydesafio.application.email.service.EmailService;
+import com.picpaydesafio.demopicpaydesafio.domain.transction.model.Transaction;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SendEmailUseCase {
+
+  private final EmailService emailService;
+
+  public void execute(Transaction transaction) {
+    emailService.sendEmail(transaction);
+  }
+}
