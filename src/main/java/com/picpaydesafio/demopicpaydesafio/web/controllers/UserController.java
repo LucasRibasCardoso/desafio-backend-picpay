d894f031ac090dec06d7657faf47393ee8844f34
Lucas Cardoso <lucas.rib.card@gmail.com>
2024-11-06 18:42:55 -0300

    feat: create method getAllUser

--- src/main/java/com/picpaydesafio/demopicpaydesafio/web/controllers/UserController.java
+++ src/main/java/com/picpaydesafio/demopicpaydesafio/web/controllers/UserController.java
@@ -4,9 +4,11 @@ import com.picpaydesafio.demopicpaydesafio.application.services.UserService;
 import com.picpaydesafio.demopicpaydesafio.web.dtos.UserRequestDTO;
 import com.picpaydesafio.demopicpaydesafio.web.dtos.UserResponseDTO;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +25,11 @@ public class UserController {
   public ResponseEntity<UserResponseDTO> insertNewUser(@RequestBody @Valid UserRequestDTO userRequest) {
     UserResponseDTO newUserResponse = userService.saveNewUser(userRequest);
     return new ResponseEntity<>(newUserResponse, HttpStatus.CREATED);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
+    List<UserResponseDTO> users = userService.findAllUsers();
+    return new ResponseEntity<>(users, HttpStatus.OK);
   }
 }
