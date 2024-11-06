847a37c991c437d21d69acb667d9d73a721f28c1
Lucas Cardoso <lucas.rib.card@gmail.com>
2024-11-06 18:45:19 -0300

    feat: separate validation into methods

--- src/main/java/com/picpaydesafio/demopicpaydesafio/application/usecases/ValidateTransactionUseCase.java
+++ src/main/java/com/picpaydesafio/demopicpaydesafio/application/usecases/ValidateTransactionUseCase.java
@@ -2,17 +2,25 @@ package com.picpaydesafio.demopicpaydesafio.application.usecases;
 
 import com.picpaydesafio.demopicpaydesafio.application.exceptions.UnauthorizedTransactionException;
 import com.picpaydesafio.demopicpaydesafio.domain.models.Transaction;
+import com.picpaydesafio.demopicpaydesafio.domain.models.User;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ValidateTransactionUseCase {
 
   public void validate(Transaction transaction) {
-    if (transaction.getSender().equals(transaction.getReceiver())) {
+    validateTransactionForYourself(transaction.getSender(), transaction.getReceiver());
+    validateTransactionSenderType(transaction.getSender());
+  }
+
+  private void validateTransactionForYourself(User sender, User receiver) {
+    if (sender.equals(receiver)) {
       throw new UnauthorizedTransactionException("Usuário não pode realizar transação consigo mesmo.");
     }
+  }
 
-    if (transaction.getSender().isMerchant()) {
+  private void validateTransactionSenderType(User user) {
+    if (user.isMerchant()) {
       throw new UnauthorizedTransactionException("Lojistas não podem realizar transações.");
     }
   }
--- src/main/java/com/picpaydesafio/demopicpaydesafio/application/usecases/ValidateUserUseCase.java
+++ src/main/java/com/picpaydesafio/demopicpaydesafio/application/usecases/ValidateUserUseCase.java
@@ -1,8 +1,9 @@
 package com.picpaydesafio.demopicpaydesafio.application.usecases;
 
-import com.picpaydesafio.demopicpaydesafio.application.services.UserService;
+import com.picpaydesafio.demopicpaydesafio.application.exceptions.InvalidEmailException;
 import com.picpaydesafio.demopicpaydesafio.application.services.interfaces.EmailValidatorService;
 import com.picpaydesafio.demopicpaydesafio.domain.models.User;
+import com.picpaydesafio.demopicpaydesafio.domain.repositoriesInterfaces.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +12,30 @@ import org.springframework.stereotype.Component;
 public class ValidateUserUseCase {
 
   private final EmailValidatorService emailValidatorService;
-  private final UserService userService;
+  private final UserRepository userRepository;
 
   public void execute(User user) {
+    validateUniqueDocument(user.getDocument());
+    validateUniqueEmail(user.getEmail());
+    validateEmailFormat(user.getEmail());
+  }
 
+  private void validateUniqueDocument(String document) {
+    if (userRepository.findByDocument(document).isPresent()) {
+      throw new InvalidEmailException("O documento informado já está cadastrado. Tente utilizar outro documento.");
+    }
+  }
+
+  private void validateUniqueEmail(String email) {
+    if (userRepository.findByEmail(email).isPresent()) {
+      throw new InvalidEmailException("O e-mail informado já está cadastrado. Tente utilizar outro e-mail.");
+    }
+  }
+
+  private void validateEmailFormat(String email) {
+    if (!emailValidatorService.isValid(email)) {
+      throw new InvalidEmailException("O e-mail informado é inválido. Tente utilizar outro e-mail.");
+    }
   }
 
 }
