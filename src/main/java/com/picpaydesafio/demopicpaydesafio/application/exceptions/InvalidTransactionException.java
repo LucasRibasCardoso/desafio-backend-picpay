f335d342436820b2f150058a7c911ea968bf7aaf
Lucas Cardoso <lucas.rib.card@gmail.com>
2024-11-29 18:20:19 -0300

    delete: delete exception

--- src/main/java/com/picpaydesafio/demopicpaydesafio/application/exceptions/InvalidTransactionException.java
+++
@@ -1,9 +0,0 @@
-package com.picpaydesafio.demopicpaydesafio.application.exceptions;
-
-public class InvalidTransactionException extends RuntimeException {
-
-  public InvalidTransactionException(String message) {
-    super(message);
-  }
-
-}
